e9f308cf2d985f3612034d8bdf7ebdc0d676907f
Danilo Vukovic <ibanez1222@gmail.com>
2021-11-26 10:46:00 +0100

    osposobljeno

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using dwprbz.Data;
+using Newtonsoft.Json;
+
+namespace dwprbz.Controllers
+{
+
+    [ApiController]
+    [Route("api/items")]
+    public class ItemsController : Controller
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            
+            using (MySqlConnection conn = new MySqlConnection("server=localhost; user=root; database=petstoredemo; port=3306; password=admin"))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM items", conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+
+                if (reader.HasRows)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+
+                    string json = JsonConvert.SerializeObject(dt);
+
+                    return json;
+
+                }
+                else return "";
+                    
+
+
+
+               /* while (reader.Read())
+                {
+
+                }*/
+
+            }
+
+
+
+         //   return
+        }
+    }
+}
---
+++ Data/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dwprbz.Data
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+    }
+}
